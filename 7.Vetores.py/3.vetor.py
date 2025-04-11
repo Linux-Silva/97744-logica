0887d2b7987fc84743135a15d65cce1bc7ddaf83
Linux-Silva <felipe.s.soares@ba.estudante.senai.br>
2025-04-11 19:11:27 +0000

    save

--- 7.Vetores.py/3.vetor.py
+++ 7.Vetores.py/3.vetor.py
@@ -2,15 +2,28 @@ import os
 os.system("cls || clear")
 
 numeros = []
+pares = 0
+impares = 0
 
-print("Lista numerica <>")
-for i in range(5):
-    num = int(input("Digite um numero: "))
+print("===Lista numérica <>===")
+for i in range(6):
+    num = int(input("Digite um número: "))
     numeros.append(num)
-    
-maior = max(numeros)
-menor = min(numeros)
-    
+
+
+for num in numeros:
+    if num % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+
 print("\nOs números informados:", numeros)
-print("O menor número é:", menor)
-print("O maior número é:", maior)
+
+for num in numeros:
+    if num % 2 == 0:
+        print(f"O número {num} é par.")
+    else:
+        print(f"O número {num} é ímpar.")
+print()
+print(f"Quantidade de números pares: {pares}")
+print(f"Quantidade de números ímpares: {impares}")
