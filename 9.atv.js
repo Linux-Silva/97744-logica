4a71abe54c19528e6d64ce7209cc509dcf894d45
Linux-Silva <felipe.s.soares@ba.estudante.senai.br>
2025-05-29 16:37:10 -0300

    Add files via upload

---
+++ 9.atv.js
@@ -0,0 +1,10 @@
+let idade = 20; 
+
+if (idade < 16) {
+  console.log("Não pode votar");
+} else if (idade >= 18 && idade <= 65) {
+  console.log("Voto obrigatório");
+} else {
+  console.log("Voto opcional");
+}
+14
